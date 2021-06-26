80c80007e92f69901e26ed717560ef61b5e08b76
Tatum Bray <tatumrbray@gmail.com>
2021-06-26 17:46:36 -0600

    transferred files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Container, Row, Col } from "reactstrap"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
+import PictureHeader from "./Components/PictureHeader"
+import AboutSection from "./Components/AboutSection"
+import SkillsSection from "./Components/SkillsSection"
+import ProjectsSection from "./Components/ProjectsSection"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Container fluid>
+        <PictureHeader />
+        <Switch>
+          <Route path='/about' component={AboutSection}></Route>
+        </Switch>
+
+        <SkillsSection />
+        <ProjectsSection />
+        <Row className='row' id='contactSect'>
+          YOYOY
+        </Row>
+        <Row className='row' id='footerSect'>
+          <div>
+            Icons made by{" "}
+            <a href='https://www.freepik.com' title='Freepik'>
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href='https://www.flaticon.com/' title='Flaticon'>
+              www.flaticon.com
+            </a>
+          </div>
+        </Row>
+      </Container>
+    </Router>
+  )
 }
 
-export default App;
+export default App
