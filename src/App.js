fc6e86f4e7611870a4c53b7cba2ca9d81332d43a
Tatum Bray <tatumrbray@gmail.com>
2021-06-26 15:04:08 -0600

    git test, fresh project files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import "./App.css"
-import { Container, Row, Col } from "reactstrap"
-
-import PictureHeader from "./Components/PictureHeader"
-import AboutSection from "./Components/AboutSection"
-import SkillsSection from "./Components/SkillsSection"
-import ProjectsSection from "./Components/ProjectsSection"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Container fluid>
-      <PictureHeader />
-      <AboutSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <Row className='row' id='contactSect'>
-        YOYOY
-      </Row>
-      <Row className='row' id='footerSect'>
-        <div>
-          Icons made by{" "}
-          <a href='https://www.freepik.com' title='Freepik'>
-            Freepik
-          </a>{" "}
-          from{" "}
-          <a href='https://www.flaticon.com/' title='Flaticon'>
-            www.flaticon.com
-          </a>
-        </div>
-      </Row>
-    </Container>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
